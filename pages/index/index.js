73af8ad93d119457336898dbe141eab6e09e5dbb
Keith <li.liang@lifecycle.cn>
2016-10-10 08:58:32 +0800

    天气Demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -42,21 +42,25 @@ Page({
       var name = this.data.name;
       var pwd = this.data.password;
       if(name === "name" && pwd === "123456"){
-          this.setData({
-              toastHidden:false,
-              toast_title:'登录成功'
+        //   this.setData({
+        //       toastHidden:false,
+        //       toast_title:'登录成功'
+        //   })
+          wx.navigateTo({
+              url:"weather"
           })
+
       }else{
           this.setData({
               toastHidden:false,
               toast_title:'用户名／密码错误'
           })
       }
-      var that = this;
-      setTimeout(function(){
-          that.setData({
-              toastHidden:true
-          })
-      },3000);
+    //   var that = this;
+    //   setTimeout(function(){
+    //       that.setData({
+    //           toastHidden:true
+    //       })
+    //   },3000);
   },
 })
